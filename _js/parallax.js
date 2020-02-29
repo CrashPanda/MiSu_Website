8eb3c0462f1d2de7be60de98a8a024d725660c03
Samantha Williams <samdaize@hotmail.com>
2020-02-29 16:49:15 +0000

    renamed assets, added a .js file, and modified index and .css

---
+++ _js/parallax.js
@@ -0,0 +1,26 @@
+// Parallax JavaScript Document
+
+var input = {
+    mouseX: {
+        start: 100,
+        end: window.innerWidth,
+        current: 0,
+    },
+    mouseY: {}
+};
+
+input.mouseX.range = input.mouseX.end - input.mouseX.start;
+
+var handleMouseMove = function (event) {
+    input.mouseX.current = event.clientX;
+    input.mouseX.fraction = (input.mouseX.current - input.mouseX.start) / input.mouseX.range;
+    console.log('fraction', input.mouseX.fraction)
+}
+
+var handleResize = function (event) {
+    input.mouseX.end = window.innerWidth;
+    input.mouseX.range = input.mouseX.end - input.mouseX.start;
+}
+
+window.addEventListener('mousemove', handleMouseMove);
+window.addEventListener('resize', handleResize);
